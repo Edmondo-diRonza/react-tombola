ab7e2861c36cec3509a0452e53a127106e05f75f
Edmondo-diRonza <eddironz@hotmail.com>
2021-12-27 15:24:17 +0100

    aggiunta parziale gestione delle vincite

--- src/context/Numbers.js
+++ src/context/Numbers.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useRef } from "react";
 import { numberExtraction } from "../util/numberExtraction";
 import { chooseCartella, chooseRowCartella } from "../util/selectCartella";
 import { speakNow } from "../util/speakNow";
@@ -16,8 +16,10 @@ export default function NumberProvider({ children }) {
   ]);
 
   const [thisGameNumbers, setThisGameNumbers] = useState(); // memorizzo il pool dei numeri estratti in questa partita
-
   const [indexOfExtracted, setIndexOfExtracted] = useState(0); //indice estrazione corrente
+  const winType = useRef(2);
+  const winningList = [null, null, "AMBO", "TERNO", "QUATERNA", "CINQUINA"];
+  const winningArray = useRef([]);
 
   const startGame = () => {
     setThisGameNumbers(numberExtraction(90));
@@ -32,10 +34,6 @@ export default function NumberProvider({ children }) {
     console.log("Partita inizializzata!");
   };
 
-  const winCheck = (cartella, riga, number = 2) => {
-    console.log(isCalled[cartella][riga].length);
-  };
-
   const callNumbers = () => {
     if (!!thisGameNumbers) {
       if (indexOfExtracted <= 89) {
@@ -44,17 +42,37 @@ export default function NumberProvider({ children }) {
         speakNow(extracted);
         let cartella = chooseCartella(extracted); //cerco cartella su cui memorizzare
         let riga = chooseRowCartella(extracted); //cerco riga su cui memorizzare
-
         let backupObjArray = JSON.parse(JSON.stringify(isCalled)); //TROVARE ALTRA SOLUZIONE QUI, NON VA BENE!?!?!?!?
-
         backupObjArray[cartella][riga].push(extracted); //aggiungo l'estratto
         setIsCalled(backupObjArray);
+
+        const currentWin = winCheck(cartella, riga, extracted);        
+        if (currentWin){ //controllo se c'è una vincita
+        winningArray.current.push(currentWin); //creo un array con storico vincite
+        speakNow(currentWin.winType);
+        console.log(winningArray);
+        }
       } else console.log("Ultimo numero già estratto!");
     } else console.log("Devi prima inizializzare la partita!");
   };
 
   const searchNumber = (number) => {
     return thisGameNumbers.numbers.indexOf(number) + 1;
+  };
+
+  const winCheck = (cartella, riga, lastNumber) => {
+    let length = isCalled[cartella][riga].length + 1;
+    if (length === winType.current) {
+      winType.current++;
+      return {
+        winType: winningList[winType.current - 1],
+        cartella,
+        riga,
+        vincenti: [...isCalled[cartella][riga], lastNumber],
+        indexOfExtracted,
+      };
+    }
+    return false;
   };
 
   return (
@@ -64,7 +82,6 @@ export default function NumberProvider({ children }) {
         setIsCalled,
         startGame,
         callNumbers,
-        winCheck,
         searchNumber,
       }}
     >
